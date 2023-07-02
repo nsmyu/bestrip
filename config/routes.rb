8e729702f2667f177f639fac83fc520cd5b84820
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-02 09:38:32 +0900

    add/usersコントローラー及びルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 end
