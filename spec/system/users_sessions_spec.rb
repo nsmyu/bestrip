1b1839855556e8beabda7c9230d0ad2edefb5d89
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-10 22:21:05 +0900

    add/ログイン失敗時のアラート設定、ログアウトのテストを追加

--- spec/system/users_sessions_spec.rb
+++ spec/system/users_sessions_spec.rb
@@ -1,60 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSessions", type: :system do
-
   let(:user) { create(:user) }
 
-  scenario "正しいメールアドレスとパスワードでログイン" do
+  before do
     visit new_user_session_path
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
-    click_button "ログイン"
+  end
 
-    expect(current_path).to eq root_path
+  describe "ログイン" do
+    context '有効な値の場合' do
+      it "ログインに成功する" do
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: user.password
+        click_button "ログイン"
 
-      aggregate_failures do
-        # expect(page).to have_content "登録完了"
-        # within ".navbar" do
-        #   expect(page).to have_content user.name
-        # end
+        aggregate_failures do
+          expect(current_path).to eq root_path
+          expect(page).not_to have_selector '.alert-danger'
+          expect(page).not_to have_selector "a[href=\"#{new_user_session_path}\"]"
+          expect(page).to have_selector "a[href=\"#{destroy_user_session_path}\"]"
+        end
       end
+    end
+
+    context 'メールアドレスもパスワードも無効な値の場合' do
+      it 'ログインに失敗し、フラッシュメッセージが表示されること' do
+        fill_in "メールアドレス", with: ""
+        fill_in "パスワード", with: ""
+        click_button "ログイン"
+
+        expect(current_path).to eq new_user_session_path
+        expect(page).to have_selector '.alert-danger'
+
+        visit root_path
+        expect(page).not_to have_selector '.alert-danger'
+      end
+    end
+
+    context 'メールアドレスは正しいが、パスワードが間違っている場合' do
+      it 'ログインに失敗し、フラッシュメッセージが表示されること' do
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: "wrongpassword"
+        click_button "ログイン"
+
+        expect(current_path).to eq new_user_session_path
+        expect(page).to have_selector '.alert-danger'
+
+        visit root_path
+        expect(page).not_to have_selector '.alert-danger'
+      end
+    end
   end
 
-  scenario "正しいメールアドレス、間違ったパスワードでログイン" do
-    visit new_user_session_path
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: "wrongpassword"
-    click_button "ログイン"
-
-    expect(current_path).to eq new_user_session_path
-
-    # aggregate_failures do
-    #   expect(page).to have_content("アカウント登録")
-    #   expect(page).to have_content("メールアドレスは不正な値です")
-    #   expect(page).to have_content("パスワードは6文字以上で入力してください")
-    #   expect(page).to have_content("パスワード（確認用）とパスワードの入力が一致しません")
-    #   within ".navbar" do
-    #     expect(page).not_to have_content user.name
-    #   end
-    # end
-  end
-
-  scenario "ログイン失敗" do
-    visit new_user_session_path
-    fill_in "メールアドレス", with: ""
-    fill_in "パスワード", with: ""
-    click_button "ログイン"
-
-    expect(current_path).to eq new_user_session_path
-
-    # aggregate_failures do
-    #   expect(page).to have_content("アカウント登録")
-    #   expect(page).to have_content("メールアドレスは不正な値です")
-    #   expect(page).to have_content("パスワードは6文字以上で入力してください")
-    #   expect(page).to have_content("パスワード（確認用）とパスワードの入力が一致しません")
-    #   within ".navbar" do
-    #     expect(page).not_to have_content user.name
-    #   end
-    # end
+  describe "ログアウト" do
+    it 'ログアウトに成功すること' do
+      sign_in user
+      visit root_path #あとで修正
+      first(:link, 'ログアウト').click
+      expect(current_path).to eq root_path
+      expect(page).to have_selector "a[href=\"#{new_user_session_path}\"]"
+      expect(page).not_to have_selector "a[href=\"#{destroy_user_session_path}\"]"
+    end
   end
 end
