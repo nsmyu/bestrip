fef4208f5a1201d92dd2387d2e457295687d20b0
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-11 07:21:52 +0900

    add/sessionsリクエストスペックを実装

--- spec/requests/users_sessions_spec.rb
+++ spec/requests/users_sessions_spec.rb
@@ -1,20 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSessions", type: :request do
-
-  let(:user) { build(:user) }
+  let(:user) { create(:user) }
 
   describe "GET /users_sessions" do
     it "ログイン画面の表示に成功すること" do
       get new_user_session_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status 200
     end
   end
 
-  # describe "POST /users_sessions" do
-  #   it "ログインに成功すること" do
-  #     post new_user_session_path, params: { user: user }
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+  describe "POST /users_sessions" do
+    it "ログインに成功すること" do
+      post user_session_path, params: { user: { email: user.email, password: user.password } }
+      expect(response).to redirect_to root_path
+    end
+
+    it "ログインに失敗すること" do
+      post user_session_path, params: { user: { email: "", password: "" } }
+      expect(response).to have_http_status 422
+    end
+  end
+
+  describe "DELETE /users_sessions" do
+    it "ログアウトに成功すること" do
+      sign_in user
+      delete destroy_user_session_path
+      expect(response).to redirect_to root_path #修正する
+    end
+  end
 end
