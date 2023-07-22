98a991e95d2431e19fd296c4e11b664b81275a99
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-22 14:48:25 +0900

    add/プロフィール編集のためのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get "users/guest_sign_in", to: 'users/sessions#guest_sign_in'
+    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     get 'users/edit_password', to: 'users/registrations#edit'
-    get 'users/edit_email', to: 'users/registrations#edit_email'
-    get 'users/profile', to: 'users/registrations#profile'
-    get 'users/profile/edit', to: 'users/registrations#profile_edit'
+    get 'users/edit_email',    to: 'users/registrations#edit_email'
+    get 'users/profile',       to: 'users/registrations#profile'
+    get 'users/edit_profile',  to: 'users/registrations#edit_profile'
   end
 end
