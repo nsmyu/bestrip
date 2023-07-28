f29ab956b1e99868b0361636182e13c9ea2a3fe2
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-28 18:14:32 +0900

    change/メールアドレスのエラーメッセージ改善

--- spec/system/users_registrations_spec.rb
+++ spec/system/users_registrations_spec.rb
@@ -3,10 +3,12 @@ require "rails_helper"
 RSpec.describe "UsersRegistrations", type: :system do
   describe "新規アカウント登録" do
     let(:user) { build(:user) }
+    before do
+      visit new_user_registration_path
+    end
 
     it "有効な値の場合、成功すること" do
       expect {
-        visit new_user_registration_path
         fill_in "ニックネーム", with: user.name
         fill_in "メールアドレス", with: user.email
         fill_in "パスワード", match: :first, with: user.password
@@ -24,9 +26,8 @@ RSpec.describe "UsersRegistrations", type: :system do
       }.to change(User, :count).by(1)
     end
 
-    it "無効な値の場合、失敗すること" do
+    it "無効な形式の場合、失敗すること" do
       expect {
-        visit new_user_registration_path
         fill_in "ニックネーム", with: ""
         fill_in "メールアドレス", with: "invalid.email"
         fill_in "パスワード", match: :first, with: "foo"
@@ -38,6 +39,25 @@ RSpec.describe "UsersRegistrations", type: :system do
           expect(page).to have_content "メールアドレスを正しく入力してください"
           expect(page).to have_content "パスワードは6文字以上で入力してください"
           expect(page).to have_content "パスワード（確認用）とパスワードの入力が一致しません"
+          within ".navbar" do
+            expect(page).not_to have_content user.name
+          end
+        end
+      }.not_to change(User, :count)
+    end
+
+    it "メールアドレスが重複している場合、失敗すること" do
+      user.save
+      expect {
+        fill_in "ニックネーム", with: "other_user"
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", match: :first, with: "password"
+        fill_in "パスワード（確認用）", with: "password"
+        click_button "新規登録"
+
+        aggregate_failures do
+          expect(page).to have_content "アカウント登録"
+          expect(page).to have_content "このメールアドレスはすでに登録されています"
           within ".navbar" do
             expect(page).not_to have_content user.name
           end
