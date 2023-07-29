460992d4e9e7ab4236b6ef8614fa916233d6affc
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-29 19:17:32 +0900

    add/itineraryのindexページのレイアウトを設定

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,7 +1,7 @@
 class Itinerary < ApplicationRecord
-  belongs_to :owner, class_name: User, foreign_key: :user_id
+  belongs_to :owner, class_name: 'User', foreign_key: :user_id
 
-  validates :name,           presence: true, length: { maximum: 50 }
+  validates :name,           presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
   validates :departure_date, presence: true
   validates :return_date,    presence: true
   validate :return_date_must_be_after_departure_date
@@ -13,5 +13,7 @@ class Itinerary < ApplicationRecord
       errors.add(:check_out_on, "は帰宅日の翌日以降で選択してください")
     end
   end
+
+  mount_uploader :image, ItineraryImageUploader
 end
 
