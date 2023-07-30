50d45634d8a8318c57f123e109c577174268871e
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-07-30 21:40:57 +0900

    add/user_sessionsのリクエストスペック異常系のテストを追加

--- spec/requests/users_sessions_spec.rb
+++ spec/requests/users_sessions_spec.rb
@@ -4,21 +4,27 @@ RSpec.describe "UsersSessions", type: :request do
   let(:user) { create(:user) }
 
   describe "GET #new" do
-    it "ログイン画面の表示に成功すること" do
+    it "正常にレスポンスを返すこと" do
       get new_user_session_path
       expect(response).to have_http_status 200
     end
   end
 
   describe "POST #create" do
-    it "ログインに成功すること" do
+    it "有効な値の場合、ログインに成功すること" do
       post user_session_path, params: { user: { email: user.email, password: user.password } }
       expect(response).to redirect_to root_path
     end
 
-    it "ログインに失敗すること" do
-      post user_session_path, params: { user: { email: "", password: "" } }
-      expect(response).to have_http_status 422
+    it "メールアドレスが間違っている場合、ログインに失敗すること" do
+      post user_session_path,
+        params: { user: { email: "wrong@example.com", password: user.password } }
+      expect(response.body).to include "メールアドレスまたはパスワードが違います。"
+    end
+
+    it "パスワードが間違っている場合、ログインに失敗すること" do
+      post user_session_path, params: { user: { email: user.email, password: "wrongpassword" } }
+      expect(response.body).to include "メールアドレスまたはパスワードが違います。"
     end
   end
 
