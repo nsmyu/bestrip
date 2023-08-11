4f6382890a9f194adc84b3ec01885031efc41968
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-08-11 19:45:49 +0900

    add/seed.rbでユーザーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create!(
+  name: "Luke",
+  bestrip_id: "luke_id",
+  email: "star_wars@example.com",
+  avatar: File.open("./app/assets/images/avatar/luke.jpg"),
+  introduction: Faker::Movies::StarWars.quote(character: "luke_skywalker"),
+  password: "password",
+  password_confirmation: "password",
+)
