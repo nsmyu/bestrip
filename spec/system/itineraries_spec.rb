2f2f5b593eab7a2349cde4fdaade4ad871e651cf
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-08-17 19:16:07 +0900

    fix/システムスペックから不要なマッチャを削除、Github Actionsのテストが落ちる原因では無かったため

--- spec/system/itineraries_spec.rb
+++ spec/system/itineraries_spec.rb
@@ -132,11 +132,8 @@ RSpec.describe "Itineraries", type: :system do
 
       it "出発日より前の日付は帰宅日として選択できないこと" do
         find("#departure-date-pickr").click
-        expect(page).to have_css '.flatpickr-calendar'
         find('div.dayContainer > span:nth-child(2)').click
-
         find("#return-date-pickr").click
-        expect(page).to have_css '.flatpickr-calendar'
         find('div.dayContainer > span:nth-child(1)').click
 
         click_on "保存する"
@@ -198,11 +195,8 @@ RSpec.describe "Itineraries", type: :system do
 
       it "出発日より前の日付は帰宅日として選択できないこと" do
         find("#departure-date-pickr").click
-        expect(page).to have_css '.flatpickr-calendar'
         find('div.dayContainer > span:nth-child(2)').click
-
         find("#return-date-pickr").click
-        expect(page).to have_css '.flatpickr-calendar'
         find('div.dayContainer > span:nth-child(1)').click
 
         click_on "保存する"
