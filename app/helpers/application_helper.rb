2327571357512f455708bb613f216e56b79a3d3a
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-10-16 10:45:04 +0900

    add/post一覧ページのviewを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     sorted_members = itinerary.members.order("itinerary_users.created_at")
     sorted_members.map { |i| i.name }.join(", ")
   end
+
+  def posted_date(post)
+    "#{post.created_at.year}.#{post.created_at.month}.#{post.created_at.day}"
+  end
 end
