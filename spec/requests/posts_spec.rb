caeb19bc084110b2a44470456ce0f62cbd476570
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-10-18 19:00:03 +0900

    change/postsリクエストスペックの変数名をより意図が伝わりやすいよう変更
    メソッドとの区別のためでありwith_photoは強調するためではない

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "Posts", type: :request do
   let!(:user) { create(:user) }
   let!(:itinerary) { create(:itinerary, :with_schedule, owner: user) }
-  let(:post_with_photo) { create(:post, :with_photo, itinerary: itinerary) }
+  # postメソッドと区別するため"test_"を付ける
+  let(:test_post) { create(:post, :with_photo, itinerary: itinerary) }
   let(:turbo_stream) { { accept: "text/vnd.turbo-stream.html" } }
 
   before do
@@ -17,18 +18,18 @@ RSpec.describe "Posts", type: :request do
     end
 
     it "投稿を全て取得すること" do
-      post_with_photo_1 = create(:post, :with_photo, itinerary: itinerary)
-      post_with_photo_2 = create(:post, :with_photo, itinerary: itinerary)
+      test_post_1 = create(:post, :with_photo, itinerary: itinerary)
+      test_post_2 = create(:post, :with_photo, itinerary: itinerary)
       get posts_path
-      expect(response.body).to include post_with_photo_1.title, post_with_photo_2.title
+      expect(response.body).to include test_post_1.title, test_post_2.title
     end
 
     it "投稿のタイトル、投稿者名、投稿日を取得すること" do
-      post_with_photo = create(:post, :with_photo, itinerary: itinerary)
+      test_post = create(:post, :with_photo, itinerary: itinerary)
       get posts_path
-      expect(response.body).to include post_with_photo.title
-      expect(response.body).to include post_with_photo.user.name
-      expect(response.body).to include date_posted(post_with_photo)
+      expect(response.body).to include test_post.title
+      expect(response.body).to include test_post.user.name
+      expect(response.body).to include date_posted(test_post)
     end
   end
 
@@ -84,7 +85,7 @@ RSpec.describe "Posts", type: :request do
 
   describe "GET #show" do
     before do
-      get post_path(id: post_with_photo.id)
+      get post_path(id: test_post.id)
     end
 
     it "正常にレスポンスを返すこと" do
@@ -92,17 +93,17 @@ RSpec.describe "Posts", type: :request do
     end
 
     it "投稿の各情報を取得すること" do
-      expect(response.body).to include post_with_photo.title
-      expect(response.body).to include post_with_photo.caption
-      expect(response.body).to include post_with_photo.user.name
-      expect(response.body).to include date_posted(post_with_photo)
-      expect(response.body).to include post_with_photo.itinerary.schedules[0].title
+      expect(response.body).to include test_post.title
+      expect(response.body).to include test_post.caption
+      expect(response.body).to include test_post.user.name
+      expect(response.body).to include date_posted(test_post)
+      expect(response.body).to include test_post.itinerary.schedules[0].title
     end
   end
 
   describe "GET #edit" do
     before do
-      get edit_post_path(id: post_with_photo.id)
+      get edit_post_path(id: test_post.id)
     end
 
     it "正常にレスポンスを返すこと" do
@@ -110,9 +111,9 @@ RSpec.describe "Posts", type: :request do
     end
 
     it "投稿の各情報を取得すること" do
-      expect(response.body).to include post_with_photo.title
-      expect(response.body).to include post_with_photo.caption
-      expect(response.body).to include post_with_photo.itinerary.title
+      expect(response.body).to include test_post.title
+      expect(response.body).to include test_post.caption
+      expect(response.body).to include test_post.itinerary.title
     end
   end
 
@@ -123,49 +124,49 @@ RSpec.describe "Posts", type: :request do
         post_params = attributes_for(:post, :with_photo, title: "Edited Title",
                                                          caption: "Edited caption.",
                                                          itinerary_id: other_itinerary.id)
-        patch post_path(id: post_with_photo.id), params: { post: post_params }
-        expect(response).to redirect_to post_path(post_with_photo.id)
-        expect(post_with_photo.reload.title).to eq "Edited Title"
-        expect(post_with_photo.reload.caption).to eq "Edited caption."
-        expect(post_with_photo.reload.itinerary_id).to eq other_itinerary.id
+        patch post_path(id: test_post.id), params: { post: post_params }
+        expect(response).to redirect_to post_path(test_post.id)
+        expect(test_post.reload.title).to eq "Edited Title"
+        expect(test_post.reload.caption).to eq "Edited caption."
+        expect(test_post.reload.itinerary_id).to eq other_itinerary.id
       end
     end
 
     context "無効な値の場合" do
       it "タイトルが空欄の場合、失敗すること" do
         post_params = attributes_for(:post, title: "")
-        patch post_path(id: post_with_photo.id), params: { post: post_params },
-                                                 headers: turbo_stream
+        patch post_path(id: test_post.id), params: { post: post_params },
+                                           headers: turbo_stream
         expect(response.body).to include "タイトルを入力してください"
       end
 
       it "タイトルが31文字以上の場合、失敗すること" do
         post_params = attributes_for(:post, title: "a" * 31)
-        patch post_path(id: post_with_photo.id), params: { post: post_params },
-                                                 headers: turbo_stream
+        patch post_path(id: test_post.id), params: { post: post_params },
+                                           headers: turbo_stream
         expect(response.body).to include "タイトルは30文字以内で入力してください"
       end
 
       it "キャプションが1001文字以上の場合、失敗すること" do
         post_params = attributes_for(:post, caption: "a" * 1001)
-        patch post_path(id: post_with_photo.id), params: { post: post_params },
-                                                 headers: turbo_stream
+        patch post_path(id: test_post.id), params: { post: post_params },
+                                           headers: turbo_stream
         expect(response.body).to include "入力内容に誤りがあります。赤字箇所をご確認ください。"
       end
 
       it "itineraryが選択されていない場合、失敗すること" do
         post_params = attributes_for(:post, itinerary_id: nil)
-        patch post_path(id: post_with_photo.id), params: { post: post_params },
-                                                 headers: turbo_stream
+        patch post_path(id: test_post.id), params: { post: post_params },
+                                           headers: turbo_stream
         expect(response.body).to include "旅のプランを選択してください"
       end
 
       it "写真が1枚も選択されていない（既存の写真が削除される）場合、失敗すること" do
         photo_delete_params =
-          { photos_attributes: [id: post_with_photo.photos[0].id, _destroy: true] }
+          { photos_attributes: [id: test_post.photos[0].id, _destroy: true] }
         post_params = attributes_for(:post,).merge(photo_delete_params)
-        patch post_path(id: post_with_photo.id), params: { post: post_params },
-                                                 headers: turbo_stream
+        patch post_path(id: test_post.id), params: { post: post_params },
+                                           headers: turbo_stream
         expect(response.body).to include "写真は1枚以上選択してください"
       end
     end
@@ -178,7 +179,7 @@ RSpec.describe "Posts", type: :request do
         sign_out user
         sign_in other_user
 
-        patch post_path(id: post_with_photo.id), params: { post: post_params }
+        patch post_path(id: test_post.id), params: { post: post_params }
         expect(response).to redirect_to posts_path
       end
     end
@@ -186,7 +187,7 @@ RSpec.describe "Posts", type: :request do
 
   describe "DELETE #destroy" do
     it "成功すること" do
-      delete post_path(id: post_with_photo.id)
+      delete post_path(id: test_post.id)
       expect(response).to redirect_to posts_path
     end
 
@@ -196,7 +197,7 @@ RSpec.describe "Posts", type: :request do
       sign_out user
       sign_in other_user
 
-      delete post_path(id: post_with_photo.id)
+      delete post_path(id: test_post.id)
       expect(response).to redirect_to posts_path
     end
   end
