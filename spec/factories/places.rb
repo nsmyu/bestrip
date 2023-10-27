8d5e3f8baaebbe6e849a54397f711b1aa99970c6
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-10-27 19:17:14 +0900

    add/placeのモデルスペックを実装、不要なファイルの削除

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,6 +1,29 @@
 FactoryBot.define do
-  factory :place do
-    place_id { "MyString" }
-    placeable { nil }
+  factory :user_place, class: "Place" do
+    sequence(:place_id) { |n| "place_id_#{n}" }
+    placeable_type { "User" }
+    association :placeable, factory: :user
+
+    trait :opera_house do
+      place_id { "ChIJ3S-JXmauEmsRUcIaWtf4MzE" }
+    end
+
+    trait :queen_victoria_building do
+      place_id { "ChIJISz8NjyuEmsRFTQ9Iw7Ear8" }
+    end
+  end
+
+  factory :itinerary_place, class: "Place" do
+    sequence(:place_id) { |n| "place_id_#{n}" }
+    placeable_type { "Itinerary" }
+    association :placeable, factory: :itinerary
+
+    trait :opera_house do
+      place_id { "ChIJ3S-JXmauEmsRUcIaWtf4MzE" }
+    end
+
+    trait :queen_victoria_building do
+      place_id { "ChIJISz8NjyuEmsRFTQ9Iw7Ear8" }
+    end
   end
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Place, type: :model, focus: true do
+  it "place_idがあり、placeableに関連付けられていれば有効であること" do
+    expect(build(:user_place)).to be_valid
+  end
+
+  it "place_idがなければ無効であること" do
+    user_place = build(:user_place, place_id: nil)
+    user_place.valid?
+    expect(user_place.errors).to be_of_kind(:place_id, :blank)
+  end
+
+  it "同じplaceable関連付けでplace_idが重複している場合は無効であること" do
+    user_place = create(:user_place)
+    other_user_place =
+      build(:user_place, place_id: user_place.place_id,
+                         placeable: user_place.placeable)
+    other_user_place.valid?
+    expect(other_user_place.errors).to be_of_kind(:place_id, :taken)
+  end
+
+  it "placeableに関連付いていなければ無効であること" do
+    user_place = build(:user_place, placeable: nil)
+    user_place.valid?
+    expect(user_place.errors).to be_of_kind(:placeable, :blank)
+  end
+
+  it "ひとつのplaceableにつき、301個以上の関連付けは無効であること" do
+    user = create(:user)
+    expect { create_list(:user_place, 301, placeable: user) }
+      .to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
