5c31197f849cda4e5ebfea85d0e0d80cc5217bee
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-10-27 09:39:21 +0900

    add/user, itineraryごとのplaces登録数にバリデーションを設定

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,11 @@ class Place < ApplicationRecord
   belongs_to :placeable, polymorphic: true
 
   validates :place_id, presence: true, uniqueness: { scope: [:placeable_type, :placeable_id] }
+  validate :places_per_placeable_cannot_be_more_than_three_hundreds
+
+  def places_per_placeable_cannot_be_more_than_three_hundreds
+    if Place.where(placeable_id: placeable_id).where(placeable_type: placeable_type).count >= 300
+      errors.add(:placeable_id, "スポットの登録数が上限に達しています")
+    end
+  end
 end
