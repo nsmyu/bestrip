ff6adc09607d8795d799cecc4ca4079db928d52b
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-03 20:35:36 +0900

    add/simple_formatメソッドでtext_areaの改行反映、itineraryとpostのseedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 require './db/seeds/user.rb'
 require './db/seeds/itinerary.rb'
+require './db/seeds/schedule.rb'
+require './db/seeds/post.rb'
 
--- db/seeds/post.rb
+++ db/seeds/post.rb
@@ -1,12 +1,23 @@
 Post.create!(
   [
     {
-      title: "シドニー 5泊6日",
-      caption: "シドニーに行ってきました！\n #オーストラリア #シドニー #海外旅行",
-      created_at: "2023-11-01",
+      title: Itinerary.find(1).title,
+      caption: "シドニーに行ってきました！\n" +  "#オーストラリア #シドニー #海外旅行",
       itinerary_public: true,
+      created_at: "2023-11-01",
       user: User.find(1),
       itinerary: Itinerary.find(1),
+      photos_attributes: [
+        {
+          url: File.open("./app/assets/images/itinerary_post/disney.jpg"),
+        },
+        {
+          url: File.open("./app/assets/images/itinerary_post/disney.jpg"),
+        },
+        {
+          url: File.open("./app/assets/images/itinerary_post/disney.jpg"),
+        }
+      ]
     }
   ]
 )
