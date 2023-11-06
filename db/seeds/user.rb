6e977eea74a0c607f94d625a1fe64d90bb52f948
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-06 13:39:53 +0900

    change/seedsのユーザーのパスワードをSecureRandomで生成したものに変更

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,13 +1,14 @@
 require 'faker'
 
 10.times do |n|
+  random_pass = SecureRandom.base36
   User.create!(
     name: Faker::Name.first_name,
     bestrip_id: "user_#{n+1}_id",
     email: "email_#{n+1}@example.com",
     avatar: File.open("./app/assets/images/avatar/avatar_#{n+1}.jpg"),
     introduction: Faker::Lorem.paragraph(sentence_count: 8),
-    password: "password",
-    password_confirmation: "password",
+    password: random_pass,
+    password_confirmation: random_pass,
   )
 end
