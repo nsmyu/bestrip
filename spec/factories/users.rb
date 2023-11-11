0d9d2fb25740d70ec51a69a0fdb8bdf053289e6e
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-11 18:29:30 +0900

    add/usersのリクエストスペックを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     sequence(:email) { |n| "email_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
+    sequence(:bestrip_id) { |n| "user_#{n}_id" }
+    introduction { "Introduction of myself." }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Users", type: :request, focus: true do
+  let!(:user) { create(:user) }
+  let!(:itinerary) { create(:itinerary, owner: user) }
+  let!(:test_post) { create(:post, :with_photo, itinerary: itinerary) }
+
+  describe "GET #show" do
+    before do
+      get user_path(id: user.id)
+    end
+
+    it "正常にレスポンスを返すこと" do
+      expect(response).to have_http_status 200
+    end
+
+    it "ユーザーの各情報を取得すること" do
+      expect(response.body).to include user.name
+      expect(response.body).to include user.bestrip_id
+      expect(response.body).to include user.introduction
+    end
+
+    it "ユーザーの投稿の情報を取得すること" do
+      expect(response.body).to include test_post.title
+    end
   end
 end
