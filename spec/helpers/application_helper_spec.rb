a801726deecfcaeda1bf181a8f3a97485e464257
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-11 11:28:53 +0900

    add/ページタイトルヘルパーのspecを追加

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  let(:user) { create(:user) }
+  let(:itinerary) { create(:itinerary, owner: user) }
+
+  describe "#full_title(page_title)", focus: true do
+    context '個別のページタイトルが設定されていない場合' do
+      it "「BesTrip」を返すこと" do
+        expect(helper.full_title("")).to eq "BesTrip"
+        expect(helper.full_title(" ")).to eq "BesTrip"
+        expect(helper.full_title(nil)).to eq "BesTrip"
+      end
+    end
+
+    context '個別のページタイトルが設定されている場合' do
+      it "「個別のページタイトル | potepanec」を返すこと" do
+        expect(helper.full_title(itinerary.title + " - 旅のプラン情報")).to eq "#{itinerary.title + " - 旅のプラン情報"} | BesTrip"
+      end
+    end
+  end
+end
