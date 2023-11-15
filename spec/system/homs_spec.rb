f9c968d369b72f66e59a4ca3caccc04ef8a2de2c
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-15 09:48:25 +0900

    clean/ガイドページのリンクのテストについて、have_linkを使いより正確なテストとなるよう修正

--- spec/system/homs_spec.rb
+++ spec/system/homs_spec.rb
@@ -2,31 +2,40 @@ RSpec.describe "Homes", type: :system do
   let(:user) { create(:user) }
 
   describe "楽しみ方ガイドページ" do
-    before do
-      visit guide_path
-    end
+    context "ユーザーがログインしていない場合" do
+      before do
+        visit guide_path
+      end
 
-    it "「旅の計画を始める」をクリックすると、アカウント作成画面へ遷移すること" do
-      click_on "旅の計画を始める"
+      it "アカウント登録ページとログインページへのリンクが表示されること" do
+        expect(page).to have_link "旅の計画を始める", href: "/users/sign_up"
+        expect(page).to have_link "ゲストログイン", href: "/users/guest_sign_in"
+      end
 
-      expect(current_path).to eq new_user_registration_path
-    end
+      it "「旅の計画を始める」をクリックすると、アカウント登録ページへ遷移すること" do
+        click_on "旅の計画を始める"
 
-    it "「ゲストログイン」をクリックすると、ゲストユーザーとしてログインすること" do
-      click_on "ゲストログイン"
+        expect(current_path).to eq new_user_registration_path
+      end
 
-      expect(current_path).to eq itineraries_path
-      within "header" do
-        expect(page).to have_content "ゲスト様"
+      it "「ゲストログイン」をクリックすると、ゲストユーザーとしてログインすること" do
+        click_on "ゲストログイン"
+
+        expect(current_path).to eq itineraries_path
+        within "header" do
+          expect(page).to have_content "ゲスト様"
+        end
       end
     end
 
-    it "ユーザーがログイン済みの場合、アカウント登録/ログインのリンクは表示されないこと" do
-      sign_in user
-      visit guide_path
+    context "ユーザーがログイン済みの場合" do
+      it "アカウント登録ページとログインページへのリンクが表示されないこと" do
+        sign_in user
+        visit guide_path
 
-      expect(page).not_to have_selector "a", text: "旅の計画を始める"
-      expect(page).not_to have_selector "a", text: "ゲストログイン"
+        expect(page).not_to have_link "旅の計画を始める", href: "/users/sign_up"
+        expect(page).not_to have_link "ゲストログイン", href: "/users/guest_sign_in"
+      end
     end
   end
 end
