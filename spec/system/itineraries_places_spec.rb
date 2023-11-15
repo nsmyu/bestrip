ae185d7ab21fc62c9f14d54af34f3e85965bbc53
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-15 14:20:02 +0900

    add/itinerary_placeシステムスペックの共通処理をメソッド化、登録/削除によるボタン切り替えのテストを追加

--- spec/system/itineraries_places_spec.rb
+++ spec/system/itineraries_places_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Itineraries::Places", type: :system do
+RSpec.describe "Itineraries::Places", type: :system, focus: true do
   let(:user) { create(:user) }
   let(:other_user) { create(:user) }
   let(:itinerary) { create(:itinerary, owner: user) }
@@ -65,7 +65,7 @@ RSpec.describe "Itineraries::Places", type: :system do
   end
 
   describe "スポット検索", js: true do
-    it "地図上の検索結果の「スポット詳細」をクリックすると、スポット情報モーダルを表示すること", focus: true do
+    it "地図上の検索結果の「スポット詳細」をクリックすると、スポット情報モーダルを表示すること" do
       visit itinerary_places_find_path(itinerary_id: itinerary.id)
       search_place_and_open_place_modal
 
@@ -77,11 +77,11 @@ RSpec.describe "Itineraries::Places", type: :system do
     end
   end
 
-  describe "行きたい場所リストへの登録", js: true, focus: true do
+  describe "行きたい場所リストへの登録", js: true do
     let(:itinerary_place) { build(:itinerary_place, :opera_house, placeable: itinerary) }
 
     context "追加登録可能な状態の場合" do
-      it "成功すること" do
+      it "成功すること（ボタンが削除用リンクに切り替わること）" do
         visit itinerary_places_find_path(itinerary_id: itinerary.id)
         search_place_and_open_place_modal
 
@@ -89,7 +89,7 @@ RSpec.describe "Itineraries::Places", type: :system do
           within ".modal" do
             click_on "行きたい場所リストに追加"
 
-            expect(page).to have_content "行きたい場所リストに追加済み"
+            expect(page).to have_xpath "//a[span[contains(text(), '行きたい場所リストに追加済み')]]"
           end
         }.to change(itinerary.places, :count).by(1)
       end
@@ -107,7 +107,7 @@ RSpec.describe "Itineraries::Places", type: :system do
         end
       end
 
-      it "上限の300件まで登録済みの場合、その旨メッセージを表示すること" do
+      it "上限の300件まで登録済みの場合、その旨メッセージを表示し、追加ボタンが無効化されていること" do
         create_list(:itinerary_place, 300, placeable: itinerary)
         visit itinerary_places_find_path(itinerary_id: itinerary.id,
                                          place_id: itinerary_place.place_id)
@@ -115,31 +115,35 @@ RSpec.describe "Itineraries::Places", type: :system do
 
         within ".modal" do
           expect(page).to have_content "行きたい場所リストの登録数が上限に達しています。"
+          expect(find(:xpath, "//button[span[contains(text(), '行きたい場所リストに追加')]]")).to be_disabled
         end
       end
     end
   end
 
-  describe "お気に入り一覧から、プランを選択して行きたい場所リストへ追加", js: true do
+  describe "お気に入りスポット一覧から行きたい場所リストへ追加", js: true do
     let!(:user_place) { create(:user_place, :opera_house, placeable: user) }
 
+    def select_itinerary_and_click_add
+      visit users_places_path
+      within(:xpath, "//div[a[p[contains(text(), 'シドニー・オペラハウス')]]]") do
+        click_on "旅のプランに追加"
+      end
+
+      within(".modal", match: :first) do
+        find(".select-box").click
+        find("option", text: itinerary.title).click
+        click_on "行きたい場所リストに追加"
+      end
+    end
+
     context "追加登録可能な状態の場合" do
-      it "成功すること（追加後、行きたい場所リストへのリンクを表示すること）" do
+      it "成功すること、行きたい場所リストへのリンクを表示すること" do
         expect {
-          visit users_places_path
-          within(:xpath, "//div[a[p[contains(text(), 'シドニー・オペラハウス')]]]") do
-            click_on "旅のプランに追加"
-          end
+          select_itinerary_and_click_add
+          expect(page).to have_content "選択したプランの行きたい場所リストに追加しました"
 
-          within(".modal", match: :first) do
-            find(".select-box").click
-            find("option", text: itinerary.title).click
-            click_on "行きたい場所リストに追加"
-
-            expect(page).to have_content "選択したプランの行きたい場所リストに追加しました"
-
-            click_on "プランを確認"
-          end
+          click_on "プランを確認"
 
           expect(page).to have_content itinerary.title
           expect(page).to have_content "シドニー・オペラハウス"
@@ -151,37 +155,21 @@ RSpec.describe "Itineraries::Places", type: :system do
     context "追加登録不可能な状態の場合" do
       it "既に追加されている場合、失敗すること" do
         create(:itinerary_place, place_id: user_place.place_id, placeable: itinerary)
+
         expect {
-          visit users_places_path
-          within(:xpath, "//div[a[p[contains(text(), 'シドニー・オペラハウス')]]]") do
-            click_on "旅のプランに追加"
-          end
+          select_itinerary_and_click_add
 
-          within(".modal", match: :first) do
-            find(".select-box").click
-            find("option", text: itinerary.title).click
-            click_on "行きたい場所リストに追加"
-
-            expect(page).to have_content "既に追加されています"
-          end
+          expect(page).to have_content "既に追加されています"
         }.not_to change(itinerary.places, :count)
       end
 
       it "上限の300件まで登録済みの場合、失敗すること" do
         create_list(:itinerary_place, 300, placeable: itinerary)
+
         expect {
-          visit users_places_path
-          within(:xpath, "//div[a[p[contains(text(), 'シドニー・オペラハウス')]]]") do
-            click_on "旅のプランに追加"
-          end
+          select_itinerary_and_click_add
 
-          within(".modal", match: :first) do
-            find(".select-box").click
-            find("option", text: itinerary.title).click
-            click_on "行きたい場所リストに追加"
-
-            expect(page).to have_content "スポットの登録数が上限に達しています"
-          end
+          expect(page).to have_content "スポットの登録数が上限に達しています"
         }.not_to change(itinerary.places, :count)
       end
     end
@@ -191,13 +179,13 @@ RSpec.describe "Itineraries::Places", type: :system do
     let!(:itinerary_place) { create(:itinerary_place, :opera_house, placeable: itinerary) }
 
     context "スポット検索画面のモーダルから削除する場合" do
-      it "成功すること" do
+      it "成功すること（ボタンが追加用フォームに切り替わること）" do
         expect {
           visit new_itinerary_place_path(itinerary_id: itinerary.id,
                                          place_id: itinerary_place.place_id)
           click_on "行きたい場所リストに追加済み", match: :first
 
-          expect(page).to have_selector ".bg-gradient-primary", text: "行きたい場所リストに追加"
+          expect(page).to have_xpath "//form[button[span[contains(text(), '行きたい場所リストに追加')]]]"
         }.to change(itinerary.places, :count).by(-1)
       end
     end
