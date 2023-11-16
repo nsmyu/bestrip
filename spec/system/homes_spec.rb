062540d01bf7d9a9999cdc0c2824e29601a2bee3
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-11-16 21:08:44 +0900

    add/トップページのイントロダクション表示についてのテストを追加

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -1,6 +1,48 @@
 RSpec.describe "Homes", type: :system do
   let(:user) { create(:user) }
 
+  describe "トップページのイントロダクション" do
+    context "ユーザーがログインしていない場合" do
+      before do
+        visit root_path
+      end
+
+      it "イントロダクションのセクションが表示されること" do
+        expect(page).to have_selector "div.page-header"
+      end
+
+      it "「BesTripの楽しみ方」をクリックすると、楽しみ方ガイドページへ遷移すること" do
+        click_on "BesTripの楽しみ方"
+
+        expect(current_path).to eq guide_path
+      end
+
+      it "「旅の計画を始める」をクリックすると、アカウント登録ページへ遷移すること" do
+        click_on "旅の計画を始める"
+
+        expect(current_path).to eq new_user_registration_path
+      end
+
+      it "「ゲストログイン」をクリックすると、ゲストユーザーとしてログインすること" do
+        click_on "ゲストログイン"
+
+        expect(current_path).to eq itineraries_path
+        within "header" do
+          expect(page).to have_content "ゲスト様"
+        end
+      end
+    end
+
+    context "ユーザーがログイン済みの場合" do
+      it "イントロダクションのセクションが表示されないこと" do
+        sign_in user
+        visit root_path
+
+        expect(page).not_to have_selector "div.page-header"
+      end
+    end
+  end
+
   describe "楽しみ方ガイドページ" do
     context "ユーザーがログインしていない場合" do
       before do
