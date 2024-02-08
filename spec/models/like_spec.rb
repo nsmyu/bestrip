a2cc6b98af9bfd3f9544cfea68dcaa370a3939b6
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2024-02-08 17:38:57 +0900

    add/Likeのモデルスペックを実装

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション", focus: true do
+    it "user_id、post_idがあれば有効であること" do
+      expect(build(:like)).to be_valid
+    end
+
+    it "user_idがなければ無効であることと" do
+      like = build(:like, user: nil)
+      like.valid?
+      expect(like.errors).to be_of_kind(:user, :blank)
+    end
+
+    it "post_idがなければ無効であることと" do
+      like = build(:like, post: nil)
+      like.valid?
+      expect(like.errors).to be_of_kind(:post, :blank)
+    end
+  end
 end
