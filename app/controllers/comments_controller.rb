58cf02e556902b4862c4cae824b70fc285a6509d
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2024-02-12 10:53:32 +0900

    add/commentsのcreateアクション→create.turbo_streamによる部分更新を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,16 @@ class CommentsController < ApplicationController
   before_action -> {
     authenticate_user!
   }
+
+  def create
+    @post = Post.find(params[:id])
+    comment = @post.comments.new(comment_params.merge(user_id: current_user.id))
+    comment.save
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
     get 'search', to: 'posts#search', on: :collection
     member do
       resources :likes, only: %i(index create destroy)
-      resources :comments, only: %i(new create edit destroy)
+      resources :comments, only: %i(create edit destroy)
     end
   end
 end
