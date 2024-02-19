bc16ffe7da65f76bd34d30912ab535cd03754fa8
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2024-02-19 13:30:48 +0900

    add/既存ユーザーが詳細された際に、ログインと同時にinvitationを承認する処理を実装

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -31,39 +31,13 @@ class Users::InvitationsController < Devise::InvitationsController
     end
   end
 
-  def update
-    @itinerary = Itinerary.find(params[:itinerary_id])
-    raw_invitation_token = update_resource_params[:invitation_token]
-    self.resource = accept_resource
-    invitation_accepted = resource.errors.empty?
-
-    yield resource if block_given?
-
-    if invitation_accepted
-      if resource.class.allow_insecure_sign_in_after_accept
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message :notice, flash_message if is_flashing_format?
-        resource.after_database_authentication
-        sign_in(resource_name, resource)
-        respond_with resource, location: after_accept_path_for(@itinerary)
-      else
-        set_flash_message :notice, :updated_not_active if is_flashing_format?
-        respond_with resource, location: new_session_path(resource_name)
-      end
-    else
-      resource.invitation_token = raw_invitation_token
-      respond_with(resource)
-    end
-  end
-
   private
 
-  def after_accept_path_for(itinerary)
-    itineraries_path(invited_to_itinerary: itinerary.id)
+  def after_accept_path_for(resource)
+    itineraries_path(invited_to_itinerary: @itinerary.id)
   end
 
   def invite_resource
-    set_itinerary
     super { |user| user.send("currently_invited_to=", @itinerary.id) }
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: :create
 
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user.invited_to_sign_up?
+      user.update_columns(invitation_accepted_at: Time.current, invitation_token: nil)
+    end
+    super
+  end
+
   def guest_sign_in
     user = User.guest
     sign_in user
