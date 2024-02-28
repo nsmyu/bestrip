41696d7c6ee62446ff2019bf4269b1573148798b
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2024-02-28 12:07:29 +0900

    fix/スマートフォンでLINE招待の画面が開かない不具合を解消

--- app/javascript/controllers/line_invitation_controller.js
+++ app/javascript/controllers/line_invitation_controller.js
@@ -2,9 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   popupWindow() {
-    const requestUrl = document.querySelector("#request_url").href
-    const wTop = window.screenTop + (window.innerHeight / 2) - 250;
-    const wLeft = window.screenLeft + (window.innerWidth / 2) - 250;
-    window.open(requestUrl, null, 'width=500, height=500, top=' + wTop + ', left=' + wLeft + ', personalbar=0, toolbar=0, scrollbars=1, resizable=!');
+    const requestUrl = document.querySelector("#request_url").href;
+    if (window.innerWidth > 950) {
+      const wTop = window.screenTop + (window.innerHeight / 2) - 250;
+      const wLeft = window.screenLeft + (window.innerWidth / 2) - 250;
+      window.open(requestUrl, null, 'width=500, height=500, top=' + wTop + ', left=' + wLeft + ', personalbar=0, toolbar=0, scrollbars=1, resizable=!');
+    } else {
+      window.location.href = requestUrl;
+    }
   }
 }
